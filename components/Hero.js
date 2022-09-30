22e5fedef855910db212622b67ffc03375f3dd86
Kris <kharonek@yahoo.com>
2022-09-30 19:49:09 +0100

    added mobile menu - no animaitons

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
 
   return (
     <div className="w-full h-screen flex justify-center">
-      <div className="text-white text-4xl mt-[30%]">
+      <div className="text-white text-4xl mt-[30%] sm:mt-[10%]">
         <h1>Hello, I&apos;m Kris</h1>
         <h2>{st}</h2>
       </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -10,30 +10,30 @@ export default function Navbar() {
 
   return (
     <div className="fixed left-0 top-0 w-full z-50 ease-in duration-300 bg-[#1F2937] text-white">
-      <div className="flex justify-between items-center p-4 text-4xl">
+      <div className="flex justify-between items-center p-4 text-4xl max-w-[1240px] m-auto">
         <div>
           <h1 className="logo">KMH</h1>
         </div>
 
-        <div onClick={handleNav}>
-          {mobileNav ? <AiOutlineClose /> : <AiOutlineMenu />}
-        </div>
-
         {/* Non-mobile Menu */}
-        <div className="hidden">
-          <ul>
+        <div className="text-xl">
+          <ul className="hidden sm:flex flex-row gap-7">
             <li>About</li>
             <li>Projects</li>
           </ul>
         </div>
-      </div>
 
-      {/* Mobile Menu */}
-      <div className="hidden">
-        <ul>
-          <li>About</li>
-          <li>Projects</li>
-        </ul>
+        {/* Mobile Menu */}
+        <div onClick={handleNav} className="sm:hidden z-10">
+          {mobileNav ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+
+        <div className={mobileNav ? "sm:hidden absolute inset-0 w-full h-screen bg-zinc-800 flex flex-col justify-center items-center text-3xl text-center ease-in duration-100" : "hidden"}>
+          <ul className="flex flex-col items-center text-4xl gap-5 bg-">
+            <li>About</li>
+            <li>Projects</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
