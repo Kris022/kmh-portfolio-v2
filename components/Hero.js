d879f9b03e42fa29c8f3c6f7e2451d491a7a019c
Kris <kharonek@yahoo.com>
2022-09-30 16:42:27 +0100

    fixing hero issue still

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="w-full h-screen flex justify-center">
       <div className="text-white text-4xl mt-[30%]">
-        <h1>Hello, I'm Kris</h1>
+        <h1>Hello, I&apos;m Kris</h1>
         <h2>{st}</h2>
       </div>
     </div>
