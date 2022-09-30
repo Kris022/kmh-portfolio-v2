c3c1f0cc6e7c76ae41f8b7e4ccb7df8f8f02c840
Kris <kharonek@yahoo.com>
2022-09-30 22:29:26 +0100

    about section added

---
+++ components/About.js
@@ -0,0 +1,25 @@
+import { AiOutlineGithub } from "react-icons/ai";
+
+export default function About() {
+  return (
+    <div className="w-full sm:w-[1000px] m-auto text-white text-2xl flex flex-col gap-8">
+        
+      <div className="text-justify tracking-tighter m-4">
+        <h1 className="text-5xl font-semibold mb-4">About</h1>
+        <p>
+          Currently on the last year of a Computer Science degree at a UK
+          university. I will be graduating in 2023 and hope to do my
+          Master&apos;s soon after.
+          <br />
+          Part time I work as a programing tutor for an online teaching start
+          up.
+        </p>
+      </div>
+
+      <div className="flex justify-center items-center flex-col text-center">
+        <AiOutlineGithub size={60} />
+        <p>GitHub</p>
+      </div>
+    </div>
+  );
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div className="fixed left-0 top-0 w-full z-50 ease-in duration-300 bg-[#1F2937] text-white">
       <div className="flex justify-between items-center p-4 text-4xl max-w-[1240px] m-auto">
         <div>
-          <h1 className="logo">KMH</h1>
+          <h1 className="logo text-5xl">KMH</h1>
         </div>
 
         {/* Non-mobile Menu */}
@@ -28,7 +28,13 @@ export default function Navbar() {
           {mobileNav ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
 
-        <div className={mobileNav ? "sm:hidden absolute inset-0 w-full h-screen bg-zinc-800 flex flex-col justify-center items-center text-3xl text-center ease-in duration-100" : "hidden"}>
+        <div
+          className={
+            mobileNav
+              ? "sm:hidden absolute inset-0 w-full h-screen bg-zinc-800 flex flex-col justify-center items-center text-3xl text-center ease-in duration-100"
+              : "hidden"
+          }
+        >
           <ul className="flex flex-col items-center text-4xl gap-5 bg-">
             <li>About</li>
             <li>Projects</li>
