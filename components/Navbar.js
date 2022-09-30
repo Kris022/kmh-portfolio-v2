d4d231ffb41af8d022b701752a87250e483187e0
Kris <kharonek@yahoo.com>
2022-09-30 23:38:07 +0100

    minor styling for the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,12 +13,12 @@ export default function Navbar() {
     <div className="fixed left-0 top-0 w-full z-50 ease-in duration-300 bg-[#1F2937] text-white">
       <div className="flex justify-between items-center p-4 text-4xl max-w-[1240px] m-auto">
         <div>
-          <Link href="/"><h1 className="logo text-5xl">KMH</h1></Link>
+          <Link href="/"><h1 className="logo text-5xl cursor-pointer">KMH</h1></Link>
         </div>
 
         {/* Non-mobile Menu */}
         <div className="text-xl">
-          <ul className="hidden sm:flex flex-row gap-7">
+          <ul className="hidden sm:flex flex-row gap-7 cursor-pointer">
             <Link href="/#about">
               <li>About</li>
             </Link>
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html>
+		<Html className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
 			<Head>
 				<link rel="stylesheet" href="..." />
 
